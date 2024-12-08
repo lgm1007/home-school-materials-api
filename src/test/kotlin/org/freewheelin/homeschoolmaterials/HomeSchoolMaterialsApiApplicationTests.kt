4b3bea43d3277bbf244439045835dc8fcce7a43c
lgm1007 <flxl654@gmail.com>
2024-12-08 15:59:08 +0900

    feat: 프로젝트 최초 생성

---
+++ src/test/kotlin/org/freewheelin/homeschoolmaterials/HomeSchoolMaterialsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.freewheelin.homeschoolmaterials
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomeSchoolMaterialsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
