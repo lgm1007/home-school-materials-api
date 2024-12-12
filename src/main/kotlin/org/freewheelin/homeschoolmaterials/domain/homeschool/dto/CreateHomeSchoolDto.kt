3d86b4eb6b031daa40e3a59b98559ef259cbb0f4
lgm1007 <flxl654@gmail.com>
2024-12-12 12:38:39 +0900

    [Modify]: API 2. 학습지 생성 시 학습지의 최대 문제 수 검사 수행

--- src/main/kotlin/org/freewheelin/homeschoolmaterials/domain/homeschool/dto/CreateHomeSchoolDto.kt
+++ src/main/kotlin/org/freewheelin/homeschoolmaterials/domain/homeschool/dto/CreateHomeSchoolDto.kt
@@ -7,6 +7,12 @@ data class CreateHomeSchoolDto(
     val teacherId: Long,
     val problemIds: List<Long>
 ) {
+    fun checkMaxProblemLength(maxLength: Int) {
+        if (problemIds.size > maxLength) {
+            throw IllegalArgumentException("학습지의 문제 수는 ${maxLength}개를 넘길 수 없습니다.")
+        }
+    }
+
     companion object {
         fun from(createRequest: CreateHomeSchoolRequest): CreateHomeSchoolDto {
             return CreateHomeSchoolDto(
