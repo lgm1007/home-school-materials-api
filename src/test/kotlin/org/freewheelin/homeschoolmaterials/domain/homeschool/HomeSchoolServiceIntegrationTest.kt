0cbf2c0a84bbf654ffe3a105f9f052d1de151c86
lgm1007 <flxl654@gmail.com>
2024-12-12 16:25:59 +0900

    modify: API 3. 학습지 출제 시 이미 해당 학습지를 출제한 학생은 출제하지 않기 조건 추가

--- src/test/kotlin/org/freewheelin/homeschoolmaterials/domain/homeschool/HomeSchoolServiceIntegrationTest.kt
+++ src/test/kotlin/org/freewheelin/homeschoolmaterials/domain/homeschool/HomeSchoolServiceIntegrationTest.kt
@@ -2,8 +2,10 @@ package org.freewheelin.homeschoolmaterials.domain.homeschool
 
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.freewheelin.homeschoolmaterials.domain.homeschool.dto.CreateHomeSchoolDto
+import org.freewheelin.homeschoolmaterials.domain.homeschool.dto.GivenHomeSchoolDto
 import org.freewheelin.homeschoolmaterials.domain.homeschool.dto.PresentHomeSchoolDto
 import org.freewheelin.homeschoolmaterials.domain.problem.HomeSchoolProblemRepository
+import org.freewheelin.homeschoolmaterials.infrastructure.homeschool.GivenHomeSchoolRepositoryImpl
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -12,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class HomeSchoolServiceIntegrationTest {
+    @Autowired
+    private lateinit var givenHomeSchoolRepositoryImpl: GivenHomeSchoolRepositoryImpl
     @Autowired lateinit var sut: HomeSchoolService
     @Autowired lateinit var homeSchoolRepository: HomeSchoolRepository
     @Autowired lateinit var homeSchoolProblemRepository: HomeSchoolProblemRepository
@@ -60,5 +64,24 @@ class HomeSchoolServiceIntegrationTest {
         assertThat(actual.size).isEqualTo(2)
         assertThat(actual[0].studentId).isEqualTo(123L)
         assertThat(actual[1].studentId).isEqualTo(456L)
+    }
+
+    @Test
+    @DisplayName("학습지 출제 중 이미 해당 학습지를 출제한 학생의 경우에는 출제하지 않는지 테스트")
+    fun createGivenHomeSchoolIsAlreadyExistTest() {
+        // 학생 ID: 123인 학생에게 이미 ID 1인 학습지 출제
+        givenHomeSchoolRepository.save(
+            GivenHomeSchoolDto(0, 1L, 123L, false)
+        )
+
+        val presentHomeSchoolDto = PresentHomeSchoolDto(
+            1L,
+            listOf(123L, 456L)
+        )
+
+        val actual = sut.createGivenHomeSchoolByStudent(presentHomeSchoolDto)
+
+        assertThat(actual.size).isEqualTo(1)
+        assertThat(actual[0].studentId).isEqualTo(456L)
     }
 }
